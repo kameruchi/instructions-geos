3b5311762668f4a73e09954740a3f71fb07a712b
Andy from TangoReloaded <andy@emplero.com>
2020-04-15 17:13:38 +0200

    fixed bug in script

--- script_files/init_script.sh
+++ script_files/init_script.sh
@@ -37,9 +37,19 @@ sudo apt install libopenmpi-dev
 
 ### Fetch files we'll need
 curl https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh > install_anaconda3-2020.02.sh
-curl -O https://github.com/kameruchi/instructions-geos/script_files/conda_env.yml
-curl https://github.com/kameruchi/instructions-geos/script_files/bash_p.sh >> .bash_profile
+curl -O https://raw.githubusercontent.com/kameruchi/instructions-geos/master/script_files/conda_env.yml
+curl https://raw.githubusercontent.com/kameruchi/instructions-geos/master/script_files/bash_p.sh >> .bash_profile
 
+
+## Bash profile
+
+#don't hardcode the Number of virtual CPUs
+#This will indicate GEOS to use ALL of the Threads / virtual CPUs
+NCPUS=$(nproc --all)
+
+echo "export OMP_NUM_THREADS=${NCPUS}" >> .bash_profile
+
+source .bash_profile
 
 #Python conda
 
@@ -51,15 +61,6 @@ conda env create -f conda_env.yml
 
 conda activate geos-python-env
 
-## Bash profile
-
-#don't hardcode the Number of virtual CPUs
-#This will indicate GEOS to use ALL of the Threads / virtual CPUs
-N_CPUS=`nproc --all`
-
-echo "export OMP_NUM_THREADS=$(N_CPUS)" >> .bash_profile
-
-source .bash_profile
 
 ## Download the GEOS Chem code
 ## Don't hardcode the version. But having it on the directory name helps for GEOS
